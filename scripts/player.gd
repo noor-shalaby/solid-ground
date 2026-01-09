78ed5fa733cb4c8558e96f2c5ab2b62233ee0006
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-09 19:44:17 +0200

    Add: variable jump height

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const SPEED: float = 600.0
 const ACCEL: float = 5000.0
 const DECEL: float = 3000.0
 const JUMP: float = -1500.0
+const JUMP_CUT_MULTIPLYER: float = 0.5
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +15,8 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= JUMP_CUT_MULTIPLYER
 	
 	var dir: float = Input.get_axis("left", "right")
 	if dir:

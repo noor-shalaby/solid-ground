2acaceac3d526f963ff2c83d211e3cb46cac2a87
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-09 19:40:27 +0200

    Add: the ability to hold down the jump button and keep jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,12 +12,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * WEIGHT * delta
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP
 	
 	var dir: float = Input.get_axis("left", "right")
 	if dir:
-		velocity.x = move_toward(velocity.x, dir * SPEED, ACCEL * delta)
+		velocity.x = move_toward(velocity.x, SPEED * dir, ACCEL * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECEL * delta)
 	

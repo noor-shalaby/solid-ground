a5d41283bc4cae3ad5b1d9773537cdc8f729993b
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-10 13:58:34 +0200

    Edit: set platforms to be spike-less by default

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -1,9 +1,15 @@
 extends AnimatableBody2D
 
 
+@export var is_trap: bool = false
 @export var spike_trigger: int = 2
 
 @onready var spike_trap: Area2D = $SpikeTrap
+@onready var player_detector: Area2D = $PlayerDetector
+
+
+func _ready() -> void:
+	player_detector.monitoring = is_trap
 
 
 func _on_player_detector_body_entered(_body: CharacterBody2D) -> void:

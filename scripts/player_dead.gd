9a5b09d209e89f5fc253bdd92f1cb323f441b8d7
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-10 13:07:28 +0200

    Add: black and white shader effect for the dead player

--- scripts/player_dead.gd
+++ scripts/player_dead.gd
@@ -1,6 +1,17 @@
 extends RigidBody2D
 
 
+@warning_ignore("unsafe_property_access")
+@onready var mat: Material = $Sprite2D.material
+
+func _ready() -> void:
+	create_tween().tween_method(set_bw, 0.0, 1.0, 0.2)
+
+
+func set_bw(value: float) -> void:
+	mat.set_shader_parameter("bw_amount", value)
+
+
 func _on_sleeping_state_changed() -> void:
 	if sleeping:
 		get_tree().call_deferred("reload_current_scene")

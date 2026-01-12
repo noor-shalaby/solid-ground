921a71545d15df869d6ccf1dca3461dabc1bd553
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-12 15:49:59 +0200

    Make player death impulse direction dependent on the spike pop direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,9 +9,13 @@ const JUMP: float = -1500.0
 const JUMP_CUT_MULTIPLYER: float = 0.5
 const COYOTE_TIME: float = 0.15
 
+const DEATH_MAX_IMPULSE: float = 2000.0
+var death_bounce_impulse: Vector2 = Vector2(randf_range(-DEATH_MAX_IMPULSE, DEATH_MAX_IMPULSE), -DEATH_MAX_IMPULSE)
+
 const DEAD_BODY_SCENE: PackedScene = preload("res://scenes/player_dead.tscn")
 
 @onready var parent: Node2D = get_parent()
+@onready var hazard_detector: Area2D = $HazardDetector
 @onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 @onready var land_sound: AudioStreamPlayer2D = $LandSound
 
@@ -43,8 +47,7 @@ func die() -> void:
 	var dead_body: RigidBody2D = DEAD_BODY_SCENE.instantiate()
 	parent.call_deferred("add_child", dead_body)
 	dead_body.global_position = global_position
-	var bounce_direction: Vector2 = Vector2(randf_range(-2000, 2000), -2000) 
-	dead_body.apply_central_impulse(bounce_direction)
+	dead_body.apply_central_impulse(death_bounce_impulse)
 	dead_body.angular_velocity = randf_range(-16.0, 16.0)
 	queue_free()
 
@@ -53,4 +56,19 @@ func _on_hazard_detector_body_entered(_body: Node2D) -> void:
 	die()
 
 func _on_hazard_detector_area_entered(_area: Area2D) -> void:
+	if _area is Spike:
+		@warning_ignore("unsafe_property_access")
+		match _area.pop_dir:
+			@warning_ignore("unsafe_property_access")
+			_area.POP_DIRS.UP:
+				death_bounce_impulse = Vector2(randf_range(-DEATH_MAX_IMPULSE, DEATH_MAX_IMPULSE), -DEATH_MAX_IMPULSE)
+			@warning_ignore("unsafe_property_access")
+			_area.POP_DIRS.DOWN:
+				death_bounce_impulse = Vector2(randf_range(-DEATH_MAX_IMPULSE, DEATH_MAX_IMPULSE), DEATH_MAX_IMPULSE)
+			@warning_ignore("unsafe_property_access")
+			_area.POP_DIRS.LEFT:
+				death_bounce_impulse = Vector2(-DEATH_MAX_IMPULSE, randf_range(-DEATH_MAX_IMPULSE, DEATH_MAX_IMPULSE))
+			@warning_ignore("unsafe_property_access")
+			_area.POP_DIRS.RIGHT:
+				death_bounce_impulse = Vector2(DEATH_MAX_IMPULSE, randf_range(-DEATH_MAX_IMPULSE, DEATH_MAX_IMPULSE))
 	die()

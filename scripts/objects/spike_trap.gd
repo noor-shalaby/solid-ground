699c4cc8bb3bf29d2127a80469dbb38ac20f20ee
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-12 19:03:59 +0200

    Use the pop trigger raycast for chasing as well and delete the chase trigger raycast

--- scripts/objects/spike_trap.gd
+++ scripts/objects/spike_trap.gd
@@ -8,7 +8,6 @@ const SPEED: float = 1000.0
 
 @onready var scene_tree: SceneTree = get_tree()
 @onready var pop_trigger: RayCast2D = $PopTrigger
-@onready var chase_trigger: RayCast2D = $ChaseTrigger
 
 var is_chasing: bool = false
 var player: CharacterBody2D = null
@@ -16,19 +15,12 @@ var player: CharacterBody2D = null
 
 func _ready() -> void:
 	pop_trigger.target_position.y = -trigger_length * Constants.TILE_SIZE
-	chase_trigger.target_position.y = -trigger_length
 
 
 func _physics_process(delta: float) -> void:
 	if pop_trigger.is_colliding():
 		pop()
 	
-	if not chase:
-		return
-	if chase_trigger.is_colliding():
-		await scene_tree.create_timer(0.1).timeout
-		is_chasing = true
-		chase_trigger.set_deferred("enabled", false)
 	if is_chasing and player:
 		if pop_dir == POP_DIRS.UP or pop_dir == POP_DIRS.DOWN:
 			position.x = move_toward(position.x, player.position.x, SPEED * delta)
@@ -40,3 +32,6 @@ func pop() -> void:
 	super()
 	player = pop_trigger.get_collider()
 	pop_trigger.set_deferred("enabled", false)
+	if chase:
+		await scene_tree.create_timer(0.2).timeout
+		is_chasing = true

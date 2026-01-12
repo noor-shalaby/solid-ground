92699ca5690710d1ee3fac03d85093ba2f3139c5
Noor Shalaby <ono.shalaby@gmail.com>
2026-01-12 21:58:07 +0200

    Add: white flash effect on player hit

--- scripts/player_dead.gd
+++ scripts/player_dead.gd
@@ -6,8 +6,16 @@ extends RigidBody2D
 @onready var death_sound: AudioStreamPlayer2D = $DeathSound
 
 func _ready() -> void:
+	blink()
 	create_tween().tween_method(set_bw, 0.0, 1.0, death_sound.stream.get_length())
 	death_sound.play()
+
+
+func blink() -> void:
+	create_tween().tween_method(set_blink_shader_intensity, 1.0, 0.0, 0.1)
+
+func set_blink_shader_intensity(new_value: float) -> void:
+	mat.set_shader_parameter("blink_intensity", new_value)
 
 
 func set_bw(value: float) -> void:
